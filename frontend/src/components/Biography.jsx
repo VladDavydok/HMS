3496dd7725c2d3ab058593c1e9f2d26564d6fb37
VladDavydok <davidok.vlad@gmail.com>
2024-12-29 12:17:35 +0200

    Commit

---
+++ frontend/src/components/Biography.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Biography = ({imageUrl}) => {
+  return (
+    <>
+      <div className="container biography">
+        <div className="banner">
+          <img src={imageUrl} alt="aboutImg" />
+        </div>
+        <div className="banner">
+          <p>Biography</p>
+          <h3>Who We Are</h3>
+          <p>
+          The company itself is a very successful company. I will open to follow the flattery of the lead. 
+          Let them flee from their debts, and the greatest consequence of this will be flexibility. 
+          From the pursuit of great pleasures, he criticizes, flees from refusal, or like that born like him, 
+          they do not know our present. Deleted, they do not know the laborious whole just!
+          </p>
+          <p>We are all in 2024!</p>
+          <p>
+          The company itself is a very successful company. We charge that the practice of taking pains is to be repelled 
+          by the choice of the pains of a corrupt body. What kind of forgiveness, even the smallest, is bound to be blinded, 
+          and to undertake great labor! They provide the gift of life less than nothing born of desire. From him, as it were, 
+          he hates nothing but pleasure to him who is very soft. Free, to assume?
+          </p>
+          <p>Thank you very much for your support!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Biography;
